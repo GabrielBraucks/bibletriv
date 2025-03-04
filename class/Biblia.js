07bab27e081f094373ecd656701a64056a27b00b
Gabriel M. Braucks <braucks.codigos@gmail.com>
2025-03-04 17:30:21 -0400

    ✨ feat: classe Biblia e arquivos .json adicionados.

---
+++ class/Biblia.js
@@ -0,0 +1,9 @@
+class Biblia {
+    constructor() {
+        let biblia_completa = {
+            aa: require('./aa.json'), acf: require('./acf.json'), nvi: require('./nvi.json')
+        };
+    }
+}
+
+export default Biblia;
